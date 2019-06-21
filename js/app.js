c0073b7f4383ea7b6134da55a649e1e17af6def4
GA Denver <gadenver@Saras-MacBook-Air.local>
2019-06-21 16:11:33 -0600

    part 1 -A

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// I. Variables & Datatypes
+// A. Q + A
+
+// How do we assign a value to a variable?
+	//1. const or let variableName = value
+
+// How do we change the value of a variable?
+	//2. variableName = newVariableValue
+
+// How do we assign an existing variable to a new variable?
+	//3. const or let newVariableName = existingVariableName 
+
+// Remind me, what are declare, assign, and define?
+	//4. Declare: to let you know what type of variable (const or let)
+	// 	 Assign: to store the value of the variable
+	//   Define: gives value 
+
+// What is pseudocoding and why should you do it?
+	//5. Its writing the coding steps out in english. This can help you to
+	//write and think through all the steps of your code
+
+// What percentage of time should be spent thinking about how youre going to 
+//solve a problem vs actually typing in code to solve it?
+	//6. 80% vs 20%
+
