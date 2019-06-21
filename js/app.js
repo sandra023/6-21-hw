3dc11bb0867d86b31502dadc8b31e674f28a27df
GA Denver <gadenver@Saras-MacBook-Air.local>
2019-06-21 17:57:07 -0600

    part 3 -D

--- js/app.js
+++ js/app.js
@@ -266,3 +266,28 @@ randomThings[2] = "World";
 // How? Why, yes! console.log();
 console.log(randomThings);
 
+// D. Change values
+// Given the following array 
+const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]
+
+// What would you write to access the 3rd element of the array?
+ourClass[2];
+
+// Change the value of "Github" to "Octocat"
+ourClass[4] = "Octocat";
+
+// Add a new element, "Cloud City" to the array.
+ourClass.push("Cloud City");
+
+console.log(ourClass);
+
+
+
+
+
+
+
+
+
+
+
