c87fff7098544a1e1bfc611d303f6eb6009936ac
GA Denver <gadenver@Saras-MacBook-Air.local>
2019-06-21 16:29:17 -0600

    part 2 - A

--- js/app.js
+++ js/app.js
@@ -109,4 +109,29 @@ if (age>= 16){
 console.log(canTheyDrive());
 
 
+// II. Loops
+// Remember: USE let when you initialize your for loops!
+// This is GOOD: for(let i = 0; i < 100; i++)
+// This is NO GOOD: for(i = 0; i < 100; i++)
+
+// A. The basics
+// Write a loop that will print out all the numbers from 0 to 10, inclusive.
+for (let i = 0; i <= 10; i++){
+	console.log(i);
+}
+
+// Write a loop that will print out all the numbers from 10 up to and 
+// including 400.
+for (let i = 10; i <=400; i++){
+	console.log(i);
+}
+
+// Write a loop that will print out every third number starting with 11 and 
+// going no higher than 4000.
+for (let i = 11; i<=4000; i+=3){
+	console.log(i)
+}
+
+
+
 
