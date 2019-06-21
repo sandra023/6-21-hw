f56db4843d764e612145b2f6cb6cf35fd27a7922
GA Denver <gadenver@Saras-MacBook-Air.local>
2019-06-21 16:12:42 -0600

    part 1 -C

--- js/app.js
+++ js/app.js
@@ -48,3 +48,37 @@ const greeting = () => {
 console.log("Hello my name is " + yourName + ".");}
 
 greeting();
+
+// C. Booleans
+// Using the provided variable definitions, replace the blanks so that all 
+// log statements print true in the console. Answers should be all be valid 
+// JS syntax and not weird things that don't make sense but happen to print 
+// true to the console.
+
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+  console.log(a < b);
+  console.log(c > d);
+  console.log('Name' === 'Name');
+  // FOR THE NEXT TWO, USE ONLY && OR ||
+  console.log(true || false);
+  console.log(false || false || false || false || false || true);
+  console.log(false === false);
+  console.log(e === 'Kevin');
+  console.log(a + b === c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression)
+  console.log(a * a === d); // note: the answer is a simple arithmetic equation, not something "weird"
+  console.log(48 == '48');
+
+// D. The farm
+// Declare a variable animal. Set it to be either "cow" or something else.
+// Write code that will print out "mooooo" if the it is equal to cow.
+// Change your code so that if the variable animal is anything other than a 
+// cow, it will print "Hey! You're not a cow."
+
+
+
+
