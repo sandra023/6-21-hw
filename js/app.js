5e138790117aa8cddf077da18d772981d11e7aef
GA Denver <gadenver@Saras-MacBook-Air.local>
2019-06-23 20:16:11 -0600

    part 5 -G

--- js/app.js
+++ js/app.js
@@ -651,13 +651,14 @@ const getRandomElement = (array) =>{
 // whatever you would like.
 
 const user = {
-	name: "Dorctor Who",
+	name: "Doctor Who",
 	email: "tardis@galifrey.com",
 	age: 912,
 	purchased: []
 }
 
 console.log(user);
+
 
 // B. Update the user
 // Our user has changed his or her email address. Without changing the 
@@ -764,16 +765,37 @@ for (let i = 0; i <user.friend.purchased.length; i++){
 
 
 
+// G. Functions can operate on objects
+// Write a single function updateUser that takes no parameters. 
+// When the function is run, it should:
+// it should increment the user's age by 1
+// make the user's name uppercase
+// The function does not need a return statement, it will merely modify 
+// the user object.
+
+const updateUser = () => {
+	user.age++;
+	user.name = user.name.toUpperCase();
+}
+
+updateUser();
+console.log(user);
 
 
+// Write a function oldAndLoud that performs the exact same tasks as 
+// updateUser, but instead of hard-coding it to only work on our user 
+// object, make it take a parameter person, and have it modify the
+// object that is passed in as an argument when the function is called. 
+// Call your oldAndLoud function with user as the argument.
 
 
+const oldAndLoud = (person) => {
+ person.age++
+ person.name = person.name.toUpperCase()
+}
 
-
-
-
-
-
+oldAndLoud(user);
+console.log(user);
 
 
 
